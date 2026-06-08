0abb2cd7ccd767d579af5538cc619dc3a48f282f
Tianyang Ren <victorren86@gmail.com>
2026-06-08 01:26:04 -0400

    Update

--- web/store/models.py
+++ web/store/models.py
@@ -17,8 +17,9 @@ class Collection(models.Model):
 # Product=========================================================================================================
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(default='-')
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
 
@@ -51,6 +52,11 @@ class Customer(models.Model):
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['last_name', 'first_name'])
+        ]
 
 # Order=========================================================================================================
 class Order(models.Model):
